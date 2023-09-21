27cfad16925b6c5f865e687903a2ea343fd23fd2
Miguel Carvalho <73550106+migueltc13@users.noreply.github.com>
2023-09-21 22:41:58 +0100

    Update flag-finder.sh. Further details in description
    - Shorten the 'find' commands. (It's faster because this way the filesystem is only iterated once)
    - Add 'type -f' to exclude directories
    - Fixed small typos

--- flag-finder/flag-finder.sh
+++ flag-finder/flag-finder.sh
@@ -4,16 +4,12 @@
 
 echo "Finding flags... Please leave this script running in a terminal as this will take a long time."
 
-find / -name "*flag.txt" -o -name ".flag*" -exec cat {} \; 2>/dev/null > temp_flags.txt
-
-find / -name ".flag" -o -name "flag" -exec cat {} \; 2>/dev/null >> temp_flags.txt
-
-find / -name "user.txt" -exec cat {} \; 2>/dev/null >> temp_flags.txt
+find / -type f -name "*flag.txt" -o -name ".flag*" -o -name "user.txt" -exec cat {} \; 2>/dev/null > temp_flags.txt
 
 grep -r "THM{" / >> temp_flags.txt 2>/dev/null
 
 if [[ -s temp_flags.txt ]]; then
-    echo "All flags Found"
+    echo "All flags found"
 else
     echo "No flags found"
 fi
@@ -22,6 +18,6 @@ sleep 2
 
 clear
 
-echo "Here is your flags."
+echo "Here are your flags."
 
 cat temp_flags.txt
